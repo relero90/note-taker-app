65dd07cec9b57dfca71bb592b41ac12d1d911439
relero90 <relero90@gmail.com>
2022-09-02 17:30:04 -0600

    /api/notes post request appends object inside object array in db.json

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const { randomUUID } = require("crypto");
+const { v4: uuidv4 } = require("uuid"); // remote module - uuid
 const express = require("express"); // remote module - express
+const chalk = require("chalk"); // remote module - chalk
+const path = require("path"); // remote module - path
 const fs = require("fs"); // shipped module - file system
 const db = require("./db/db.json"); // local database json file
 
@@ -7,6 +9,13 @@ const PORT = process.env.PORT || 3001;
 
 const app = express(); // express app
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// GET * should return the index.html file.
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+);
 // GET /notes should return the notes.html file.
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
@@ -15,40 +24,45 @@ app.get("/notes", (req, res) =>
 app.get("/api/notes", (req, res) => res.json(db));
 
 app.post("/api/notes", (req, res) => {
-  const { title, note } = req.body;
+  // Log that a POST request was received
+  console.log(chalk.cyan(`${req.method} request received to add a note.`));
+  console.log(req.body);
+  const { title, text } = req.body;
   // If all the required properties are present
-  if (title && note) {
+  if (title && text) {
     // Variable for the object we will save
     const newNote = {
       title,
-      note,
-      id = randomUUID,
+      text,
+      // id = uuid(),
     };
     //receive a new note to save on the request body
     //give each note a unique id when it's saved
     //add it to the db.json file
-    fs.readFile(db, "utf-8", (err, data) => {
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
       if (err) {
         console.log(err);
       } else {
         const savedNotes = JSON.parse(data);
         savedNotes.push(newNote);
-        fs.writeFile(db, JSON.stringify(notes), (err) =>
+        fs.writeFile("./db/db.json", JSON.stringify(savedNotes), (err) =>
           err
             ? console.error(err)
-            : console.log(`Note id ${newNote.id} has been written to JSON file.`)
+            : console.log(
+                `Note id ${newNote.id} has been written to JSON file.`
+              )
         );
       }
-    })
+    });
     //return the new note to the client
-    res.status(201).json(db)
+    res.status(201).json(db);
   } else {
     res.status(500).json("Oops! Problem saving note.");
   }
+});
 
-// GET * should return the index.html file.
-app.get("/*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
+app.listen(PORT, () =>
+  console.log(
+    chalk.blue(`Notetaker app is listening at http://localhost:${PORT} 🚀`)
+  )
 );
-
-app.listen(PORT, () => console.log(`Notetaker app is listening at http://localhost:${PORT} 🚀`))
