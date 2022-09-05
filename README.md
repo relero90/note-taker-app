4593456e1aeed816b2d47406b585c7e6b9d91f16
relero90 <relero90@gmail.com>
2022-09-04 18:39:25 -0600

    repaired file paths in apiNotes.js using path.join()

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# note-taker-app
+# Note Taker App
+
+![None of the above](https://img.shields.io/badge/license-NA-lightgrey)
+
+## Description
+
+---
+
+This is an app that allows you to save titled notes to a list. The user may click on saved notes to display their content. A delete button is provided for removing notes that are no longer needed.
+
+## Screenshot
+
+---
+
+!["A screenshot of the app showing a column of saved notes on the left, text input areas for note text and title, an add button, and a save button."]()
+
+## Contact
+
+---
+
+- GitHub: [relero90](https://github.com/relero90)
--- routes/api/apiNotes.js
+++ routes/api/apiNotes.js
@@ -1,18 +1,23 @@
 const router = require("express").Router();
 const uuid = require("uuid"); // remote module - uuid
 const fs = require("fs"); // shipped module - file system
+const path = require("path");
 const chalk = require("chalk"); // remote module - chalk
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 router.get("/api/notes", (req, res) => {
-  fs.readFile("./db/db.json", "utf-8", (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      let savedNotes = JSON.parse(data);
-      res.json(savedNotes);
+  fs.readFile(
+    path.join(__dirname, "../../db/db.json"),
+    "utf-8",
+    (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        let savedNotes = JSON.parse(data);
+        res.json(savedNotes);
+      }
     }
-  });
+  );
 });
 
 router.post("/api/notes", (req, res) => {
@@ -32,15 +37,23 @@ router.post("/api/notes", (req, res) => {
     //give each note a unique id when it's saved
     //add it to the db.json file
     let savedNotes = [];
-    fs.readFile("./db/db.json", "utf-8", (err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        savedNotes = JSON.parse(data);
-        savedNotes.push(newNote);
-        fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes), "utf-8");
+    fs.readFile(
+      path.join(__dirname, "../../db/db.json"),
+      "utf-8",
+      (err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+          savedNotes = JSON.parse(data);
+          savedNotes.push(newNote);
+          fs.writeFileSync(
+            path.join(__dirname, "../../db/db.json"),
+            JSON.stringify(savedNotes),
+            "utf-8"
+          );
+        }
       }
-    });
+    );
     //return the new note to the client
     //render the note
     res.json(savedNotes);
@@ -52,24 +65,32 @@ router.post("/api/notes", (req, res) => {
 // receive a query parameter that contains the id of a note to delete.
 router.delete("/api/notes/:id", (req, res) => {
   // read all notes from the db.json file
-  fs.readFile("./db/db.json", "utf-8", (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      let savedNotes = JSON.parse(data);
-      const reqId = req.params.id;
-      // filter savedNotes array for notes with id = user query parameter
-      for (var i = 0; i < savedNotes.length; i++) {
-        if (reqId === savedNotes[i].id) {
-          // remove the note with the given id property
-          savedNotes.splice(i, 1);
-          // then rewrite the notes to the db.json file.
-          fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes), "utf-8");
-          res.json(savedNotes);
+  fs.readFile(
+    path.join(__dirname, "../../db/db.json"),
+    "utf-8",
+    (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        let savedNotes = JSON.parse(data);
+        const reqId = req.params.id;
+        // filter savedNotes array for notes with id = user query parameter
+        for (var i = 0; i < savedNotes.length; i++) {
+          if (reqId === savedNotes[i].id) {
+            // remove the note with the given id property
+            savedNotes.splice(i, 1);
+            // then rewrite the notes to the db.json file.
+            fs.writeFileSync(
+              path.join(__dirname, "../../db/db.json"),
+              JSON.stringify(savedNotes),
+              "utf-8"
+            );
+            res.json(savedNotes);
+          }
         }
       }
     }
-  });
+  );
 });
 
 module.exports = router;
